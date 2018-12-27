c4868dda6642cb979453a990276a12fdb95c1c30
gx1989mao <gx1989mao@gmail.com>
2018-12-27 21:17:38 +0800

    entrance part

--- assets/Script/baseClassRole.js
+++ assets/Script/baseClassRole.js
@@ -2,7 +2,7 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        syntaxTree : [["start","right","right","up"]],     // when green-flag button is pressed, syntaxTree refresh 
+        syntaxTree : [],     // when green-flag button is pressed, syntaxTree refresh 
         stepPoints : [],
         hideFlag : false,    // default display
         logicTimer : 30,     // per 30 frames logic engine run once 
@@ -29,19 +29,49 @@ cc.Class({
     // run our engine, clear frame timer, refresh syntax tree 
     refreshTree: function () {
         this.frameNum = 0;
-        this.syntaxTree.forEach(v=>{this.stepPoints.push(0);});   // reset step points to zero
+        this.syntaxTree.push(["start","right","right","up"]);
+        this.syntaxTree.push(["ontouch","left","right"]);
+        this.syntaxTree.forEach(v=>{this.stepPoints.push(0);});              // reset step points to zero
     },
-    phyEngine: function () {
-        // cc.log("phy");
+
+    logicEngine: function () {     
+        this.phyAction = [];                                                 // refresh phyaction
+        this.logicEngineStep();                                              // one step of logic engine       
     },
-    logicEngine: function () {
-        cc.log("logic"+this.frameNum);
-        for(var i=0; i<this.stepPoints.length; i++){
-            if(this.syntaxTree[i][this.stepPoints[i]] === "start"){
+
+    logicEngineStep: function () {
+        // for each tree
+        for(var i=0; i<this.syntaxTree.length; i++){             
+            // register entrances
+            if(this.syntaxTree[i][this.stepPoints[i]] === "start"            && this.stepPoints[i] === 0){
                 this.stepPoints[i]++;
             }
+            else if(this.syntaxTree[i][this.stepPoints[i]] === "ontouch"     && this.stepPoints[i] === 0){
+                if("ontouch" in this.logicAction)
+                    this.stepPoints[i]++;
+            }
+            else if(this.syntaxTree[i][this.stepPoints[i]] === "oncollision" && this.stepPoints[i] === 0){
+                if("oncollision" in this.logicAction)
+                    this.stepPoints[i]++;
+            }
+            else if(this.syntaxTree[i][this.stepPoints[i]] === "onRx"        && this.stepPoints[i] === 0){
+                if("onRx" in this.logicAction)
+                    this.stepPoints[i]++;
+            }
+            // alread entered, run!
+            else if(this.stepPoints[i] !== 0){
+                if(this.stepPoints[i] < this.syntaxTree[i].length){          // if not point to the last one
+                    var action = this.syntaxTree[i][this.stepPoints[i]];
+                    this.phyAction.push(action);
+                    this.stepPoints[i]++;
+                }
+                
+            }
         };
+    },
 
+    phyEngine: function () {
+        cc.log(this.phyAction);
     },
     
 });
