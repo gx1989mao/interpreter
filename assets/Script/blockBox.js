602b4e6dcc2787fbe964866848aafa7d273b3094
gx1989mao <gx1989mao@gmail.com>
2018-12-31 17:50:41 +0800

    blockbox add blocks without change block frame

--- assets/Script/blockBox.js
+++ assets/Script/blockBox.js
@@ -7,16 +7,60 @@ cc.Class({
 
     properties:{
         sortNow: "entrance",
+        block:{
+            default: null, 
+            type: cc.Prefab, 
+        },
+        entrance: [],
+        move: [],
+        role: [],
+        voice: [],
+        time: [],
+        finish: [],
+    },
+
+    onLoad: function() {
+        this.entrance = ["11","12","13","14","15"];
+        this.move = ["21","22","23","24","25","26","27"];
+        this.role = ["31","32","33","34","35","36"];
+        this.voice = ["41"];
+        this.time =  ["51","52","531","541"];
+        this.finish = ["61","62"];
     },
 
     update: function(){
         if(boxSort.sort !== this.sortNow){
-            cc.log(boxSort.sort);
+            this.initblock(boxSort.sort);
+            //cc.log(boxSort.sort);
             this.sortNow = boxSort.sort;
         }
             
     },
-    
 
-    
+    initblock: function(s) {
+        cc.log(s);
+        var children = this.node.children;
+        children.forEach(v=>{v.destroy()});
+
+        if(s === "entrance")
+            this.entrance.forEach(v=>{this.initOneBlock(v)});
+        if(s === "move")
+            this.move.forEach(v=>{this.initOneBlock(v)});
+        if(s === "role")
+            this.role.forEach(v=>{this.initOneBlock(v)});
+        if(s === "voice")
+            this.voice.forEach(v=>{this.initOneBlock(v)});
+        if(s === "time")
+            this.time.forEach(v=>{this.initOneBlock(v)});
+        if(s === "finish")
+            this.finish.forEach(v=>{this.initOneBlock(v)});
+    },
+
+    initOneBlock: function(name) {
+        var b = cc.instantiate(this.block);
+        b.name = name;
+        // var frame = b.getComponent(cc.Sprite).atlas.getSpriteFrame(b11.name);
+        // b.getComponent(cc.Sprite).spriteFrame = frame;
+        b.parent = this.node;
+    }
 });
